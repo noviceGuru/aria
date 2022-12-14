6284dfc0ebe472d7e17a4ba18bf56aaf7a78d482
noviceGuru <amin.arabi@gmail.com>
2022-12-14 14:27:24 +0100

    third lesson files and diagrams added

--- lesson1.js
+++
@@ -1,7 +0,0 @@
-// Store the data
-let names = ['AminA', 'AminM', 'Aria', 'Pooria']
-
-// Put it in action
-for (let index = 0; index < names.length; index++) {
-    console.log(names[index])
-}
---
+++ lesson3bScope.js
@@ -0,0 +1,15 @@
+// let firstName = "Amin"
+
+// function printName(){
+//     console.log(firstName)
+// }
+
+// printName()
+
+
+
+function printName(){
+    let firstName = "Amin"
+}
+
+console.log(firstName)
