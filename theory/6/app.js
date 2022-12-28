a041ef782b0628a565181185b57b38c47b7d67ef
noviceGuru <amin.arabi@gmail.com>
2022-12-28 15:14:08 +0100

    changes added

--- theory/6/app.js
+++ theory/6/app.js
@@ -13,14 +13,14 @@ let firstNumber
 let secondNumber
 let operation
 
-const inputNumber = createElement("input", "input-number", "", document.getElementById("button-container"))
+const inputNumber = createElement("input", "input-number", "", document.body)
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-const numberButtons = numbers.map(e => createElement("button", `button-${e}`, e, document.getElementById("button-container")))
-const equalButton = createElement("button", "equal", "=", document.getElementById("button-container"))
-const clearButton = createElement("button", "clear", "C", document.getElementById("button-container"))
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13,0]
+const numberButtons = numbers.map(e => createElement("button", `button-${e}`, e, document.body))
+const equalButton = createElement("button", "equal", "=", document.body)
+const clearButton = createElement("button", "clear", "C", document.body)
 const operations = ["*", "+", "-", "/"]
-const operationsButtons = operations.map(e => createElement("button", `button-${e}`, e, document.getElementById("button-container")))
+const operationsButtons = operations.map(e => createElement("button", `button-${e}`, e, document.body))
 
 // Adding onclick functions
 numberButtons.forEach(e => e.onclick = () => {
